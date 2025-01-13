392ddae5f8ef3cbe5ff607c9d8992608c0c26913
Harold Lee <49042463+haroldlkh@users.noreply.github.com>
2025-01-13 09:46:18 -0500

    a

---
+++ mta/921m_stack.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = []
+        for c in s:
+            if c == '(': stack.append(c)
+            elif stack and stack[-1] == '(': stack.pop()
+            else: stack.append(c)
+
+        return len(stack)

a1baca393a91b016e6c58740573ec5a9984d752e
Harold Lee <49042463+haroldlkh@users.noreply.github.com>
2025-12-28 01:20:28 -0500

    a

---
+++ general/15m_custom_bsearch.py
@@ -0,0 +1,18 @@
+def count_triplets(nums, k):
+    nums.sort()
+    count = 0
+    n = len(nums)
+
+    for i in range(n-2):
+        for j in range(i+1,n-1):
+            complement = k - nums[i] - nums[j]
+            left = j+1
+            right = n-1
+            while left <= right:
+                mid = (left + right)//2
+                if nums[mid] == complement:
+                    count+=1
+                    break
+                elif nums[mid] > complement: right = mid-1
+                else: left = mid+1
+    return count
